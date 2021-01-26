00ffb611397efe12321c043ba51545eac0bad0b6
Alex Semchenkov <zigola12@list.ru>
2021-01-26 18:02:32 +0300

    First stage to same wifi multiplayer

---
+++ Assets/Scripts/StartLoadScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartLoadScript : MonoBehaviour
+{
+    [SerializeField]
+    private Slider _loadSlider;
+
+    float stepSlider = 0.1f;
+
+    [SerializeField]
+    private Text _persentText;
+
+    
+
+    
+
+    void Update()
+    {
+        _loadSlider.value += stepSlider * Time.deltaTime;
+        _persentText.text = Mathf.Round(_loadSlider.value * 100) + " %"; 
+
+
+        if (_loadSlider.value == 1)
+        {
+            NextScene();
+        }
+        GameObject[] rew = GameObject.FindGameObjectsWithTag("player");
+        for (int i = 0; i < rew.Length; i++)
+        {
+            rew[i].transform.SetParent(this.transform);
+        }
+    }
+
+    void NextScene()
+    {
+        
+         Debug.Log("Win");
+        
+    }
+    
+}
